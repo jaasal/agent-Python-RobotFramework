93b7ff69eeeeaeba041491f2a0880ee7b37af663
Jaakko Salminen <jaakko.salminen@cloubi.com>
2020-06-26 18:56:39 +0300

    Support for rerun

--- robotframework_reportportal/listener.py
+++ robotframework_reportportal/listener.py
@@ -12,6 +12,7 @@ items = []
 def start_launch(launch):
     """Start a new launch at the Report Portal."""
     if Variables.launch_rerun:
+        Variables.launch_id = Variables.launch_rerun_id
         RobotService.rp.rerun_of = Variables.launch_rerun_id
         RobotService.rp.rerun = Variables.launch_rerun
     if not Variables.launch_id:
